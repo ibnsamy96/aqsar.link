fbc743360f410316e63180f7aec323ea1ff7cf40
Mahmoud Samy <mahmoudsamy50@outlook.com>
2020-11-03 23:34:27 +0200

    removed unused functions in app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ function createNewShortLink() {
   let randomSlug;
   const domain = link.value;
 
-  generateSlug(1)
+  generateSlug(5)
     .then((slug) => {
       randomSlug = slug;
       console.log(randomSlug);
@@ -99,39 +99,8 @@ function createNewShortLink() {
     .catch((err) => console.log(err));
 }
 
-function specifyURLSlug() {
-  const slug = window.location.pathname.split("/")[1];
-  if (slug !== "" && slug !== " ") {
-    return window.location.pathname.split("/")[1];
-  }
-  return false;
-}
-
-// eslint-disable-next-line no-unused-vars
-function getShortLink(givenSlug) {
-  const slug = specifyURLSlug() || givenSlug;
-  console.log(
-    `${databaseApi}.json?orderBy="$key"&equalTo="${slug}"&print=pretty`
-  );
-
-  return getData(
-    `${databaseApi}.json?orderBy="$key"&equalTo="${slug}"&print=pretty`
-  )
-    .then((data) => {
-      console.log(data);
-      console.log(data[`${slug}`].domain);
-      return data[`${slug}`].domain;
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((data) => console.log(data));
-}
-
 // eslint-disable-next-line no-unused-vars
 function copyShortLink() {
-  const slug = result.innerText.split("/")[1];
-
   // copy link to clipboard
   const temporaryInput = document.createElement("input");
   temporaryInput.setAttribute("value", result.innerText);
@@ -143,15 +112,4 @@ function copyShortLink() {
     console.warn("Copy to clipboard failed.", error);
   }
   document.body.removeChild(temporaryInput);
-
-  getShortLink(slug);
-}
-
-// eslint-disable-next-line no-unused-vars
-async function goToLink() {
-  const slug = result.innerText.split("/")[1];
-
-  const URL = await getShortLink(slug);
-
-  window.open(URL, "_blank"); // open link in new tab
 }
