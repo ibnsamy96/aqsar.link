74636239f552a61fa3d8a3203a216fe98f5f1ea2
Mahmoud Samy <mahmoudsamy50@outlook.com>
2020-11-03 04:36:45 +0200

    added random ID generator

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ const getData = async (url = "") => {
 
 // TODO generate codes as Firebase codes has '-' and domains mustn't have one
 
+function generateID(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 const link = document.querySelector("#link");
 const result = document.querySelector("#result");
 
