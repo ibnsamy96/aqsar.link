025d8d6b344f155b298c6cb660dacfe3f89545b7
Mahmoud Samy <mahmoudsamy50@outlook.com>
2020-11-03 04:18:08 +0200

    installed eslint & prettier

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
-const databaseApi = 'https://ibn-samy-short-links.firebaseio.com/links.json';
+const databaseApi = "https://ibn-samy-short-links.firebaseio.com/links.json";
 
-const postData = async (url = '', data = {}) => {
+const postData = async (url = "", data = {}) => {
   const request = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json' },
-    mode: 'cors',
-    credentials: 'same-origin',
+    headers: { "Content-Type": "application/json" },
+    mode: "cors",
+    credentials: "same-origin",
   });
 
   return request.json();
 };
 
+const getData = async (url = "") => {
+  const request = await fetch(url, { mode: "cors", method: "GET" });
 
-const getData = async (url = '') => {
-    const request = await fetch(url, { mode: 'cors', method: 'GET' });
-  
-    return request.json();
-  };
+  return request.json();
+};
 
-
-
-  
 // TODO generate codes as Firebase codes has '-' and domains mustn't have one
 
-const link = document.querySelector('#link');
-const result = document.querySelector('#result');
-
+const link = document.querySelector("#link");
+const result = document.querySelector("#result");
 
 // eslint-disable-next-line no-unused-vars
 function createNewShortLink() {
-
   const domain = link.value;
   console.log(domain);
 
@@ -46,60 +40,54 @@ function createNewShortLink() {
 }
 
 function specifyURLSlug() {
-    const slug = window.location.pathname.split('/')[1];
-    if (slug !== '' && slug !== ' ') {
-        return window.location.pathname.split('/')[1];
-    }
-    return false
+  const slug = window.location.pathname.split("/")[1];
+  if (slug !== "" && slug !== " ") {
+    return window.location.pathname.split("/")[1];
+  }
+  return false;
 }
 
 // eslint-disable-next-line no-unused-vars
-function getShortLink(givenSlug){
+function getShortLink(givenSlug) {
+  const slug = specifyURLSlug() || givenSlug;
+  console.log(`${databaseApi}?orderBy="$key"&equalTo="${slug}"&print=pretty`);
 
-    const slug = specifyURLSlug() || givenSlug;
-    console.log(`${databaseApi}?orderBy="$key"&equalTo="${slug}"&print=pretty`);
-
-  return  getData(
-        `${databaseApi}?orderBy="$key"&equalTo="${slug}"&print=pretty`,
-      )
-        .then((data) => {
-            console.log(data);
-          console.log(data[`${slug}`].domain);
-          return data[`${slug}`].domain;
-        })
-        .then((data) => {
-            return data;
-        })
-        .catch((data) => console.log(data));
-      
+  return getData(`${databaseApi}?orderBy="$key"&equalTo="${slug}"&print=pretty`)
+    .then((data) => {
+      console.log(data);
+      console.log(data[`${slug}`].domain);
+      return data[`${slug}`].domain;
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((data) => console.log(data));
 }
 
-
 // eslint-disable-next-line no-unused-vars
-function copyShortLink(){
-  const slug =  result.innerText.split('/')[1] ;
+function copyShortLink() {
+  const slug = result.innerText.split("/")[1];
 
   // copy link to clipboard
-  const temporaryInput = document.createElement('input');
-  temporaryInput.setAttribute('value', result.innerText);
+  const temporaryInput = document.createElement("input");
+  temporaryInput.setAttribute("value", result.innerText);
   document.body.appendChild(temporaryInput);
   temporaryInput.select();
   try {
-       document.execCommand("copy");  // Security exception may be thrown by some browsers.
-  }
-  catch (error) {
-      console.warn("Copy to clipboard failed.", error);
+    document.execCommand("copy"); // Security exception may be thrown by some browsers.
+  } catch (error) {
+    console.warn("Copy to clipboard failed.", error);
   }
   document.body.removeChild(temporaryInput);
 
-  getShortLink(slug)
+  getShortLink(slug);
 }
 
+// eslint-disable-next-line no-unused-vars
+async function goToLink() {
+  const slug = result.innerText.split("/")[1];
 
-async function goToLink(){
-    const slug =  result.innerText.split('/')[1] ;
+  const URL = await getShortLink(slug);
 
-const URL= await getShortLink(slug);
-
-window.open(URL, '_blank'); // open link in new tab
+  window.open(URL, "_blank"); // open link in new tab
 }
