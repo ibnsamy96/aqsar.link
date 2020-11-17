da6e17d12589d96283b5c7e09a4cc55e91595da3
Mahmoud Samy <mahmoudsamy50@outlook.com>
2020-11-17 03:23:48 +0200

    fixed qrImg display state

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ window.createQR = async () => {
     "src",
     `https://api.qrserver.com/v1/create-qr-code/?data=${url}&size=100x100`
   );
-  qrBTN.className = '';
+  qrImg.className = '';
   qrImg.style.display = "block";
   console.log(url);
   // const request = await fetch(
