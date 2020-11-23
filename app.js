b780be3a8755695bc1582f04be15d2774167c040
Mahmoud Samy <mahmoudsamy50@outlook.com>
2020-11-23 22:10:21 +0200

    adding build.py to add hashs to files and it's paths

--- app.js
+++ app.js
@@ -21,10 +21,12 @@ const putData = async (url = "", data = {}) => {
 };
 
 function validateURL(url) {
+
   const pattern = new RegExp(
     "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$",
     "i"
   ); // fragment locator
+
   return pattern.test(url);
 }
 
