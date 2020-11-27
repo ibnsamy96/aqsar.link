429a39b2eab4a980be8b54dccc2b89c78af34d51
Mahmoud Samy <mahmoudsamy50@outlook.com>
2020-11-27 20:22:39 +0200

    fixed regex to makesure that text has it's protocol

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const putData = async (url = "", data = {}) => {
 function validateURL(url) {
 
   const pattern = new RegExp(
-    "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$",
+    "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$",
     "i"
   ); // fragment locator
 
