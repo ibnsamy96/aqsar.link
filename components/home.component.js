0f77a86ad6c54fd0b40029aa1eefe0b8c2d748e2
Mahmoud Samy <mahmoudsamy50@outlook.com>
2020-11-29 05:07:48 +0200

    finished first itirate of final design

--- components/home.component.js
+++ components/home.component.js
@@ -2,25 +2,52 @@
 export const HomeComponent = {
   render: () => {
     return `
-    <section>
-    <form action="javascript:;" onsubmit="createNewShortLink()">
-      <label for="link">Url to shorten:</label>
-      <input type="text" name="link" id="link" required />
-      <small id="urlValidity"></small>
-      <!-- <label for="slug">Optional. Custom micro url:</label>
-      <input type="text" name="slug" id="slug" /> -->
-      <input type="submit" value="Create" />
-    </form>
-  </section>
+  
+  <div id="app" class=" row m-0 h-100">
+      <main class='col my-auto'>
+          <div class="row mb-5">
+              <div class="col-12">
+                  <h1 id='' class="primaryText">أقصر لينك</h1>
+              </div>
+          </div>
+          <section id='formSection'>
+              <form>
+                  <div action="javascript:;" onsubmit="createNewShortLink()" class="form-row justify-content-center">
+                      <div class='col-5 pl-0'>
+                          <input class="form-control" type="text" name="link" id="link" required />
+                      </div>
+                      <div class='col-auto pr-0'>
+                          <input id='submitBTN' class="btn btn-block btn-primary" type="submit" value="قصّر الرابط" />
+                      </div>
+                      <small class="form-text text-muted" id="urlValidity"></small>
+                  </div>
+              </form>
+          </section>
+          <section class="row justify-content-center mt-5" id='shortenedLinks'>
+              <div class="col-md-6">
+                  <h2 id='shortenedLinksHeadline' class="">آخر الروابط المقصرة</h2>
+                  <div class='linkGroup'>
+                      <!-- Delete this paragraph -->
+                      <p id="result"></p>
+                      <!--  -->
+                      <p class="shortLink"></p>
+                      <p class="longLink"></p>
+                      <button class='copyBTN btn btn-secondary' hidden='true' onclick="copyShortLink()">Copy</button>
+                      <button class='qrBTN btn btn-secondary' hidden='true' onclick="createQR()">Create QR</button>
+                  </div>
+              </div>
+          </section>
+          <section id='qrCodeOverlay'>
+              <div id='qrBox'>
+                  <i id='closeOverlay'></i>
+                  <img src="" id="qrImg" hidden='true' />
+                  <a id='downloadQRImg'></a>
+              </div>
+          </section>
+      </main>
+  </div>
 
-  <!-- <input type="text" name="link" id="link" />
-  <button onclick="createNewShortLink()">Create</button> -->
-  <p id="result"></p>
-  <button class='displayNone' onclick="copyShortLink()">Copy</button>
-  <button class='displayNone' onclick="createQR()">Create QR</button>
-  <img src="" id="qrImg" class='displayNone' />
-
-      `;
+    `;
   },
   injectCode: () => {
     return [{
@@ -42,3 +69,27 @@ export const HomeComponent = {
     ]
   }
 };
+
+
+
+
+// return `
+// <section>
+// <form action="javascript:;" onsubmit="createNewShortLink()">
+//   <label for="link">Url to shorten:</label>
+//   <input type="text" name="link" id="link" required />
+//   <small id="urlValidity"></small>
+//   <!-- <label for="slug">Optional. Custom micro url:</label>
+//   <input type="text" name="slug" id="slug" /> -->
+//   <input type="submit" value="Create" />
+// </form>
+// </section>
+
+// <!-- <input type="text" name="link" id="link" />
+// <button onclick="createNewShortLink()">Create</button> -->
+// <p id="result"></p>
+// <button class='displayNone' onclick="copyShortLink()">Copy</button>
+// <button class='displayNone' onclick="createQR()">Create QR</button>
+// <img src="" id="qrImg" class='displayNone' />
+
+//   `;
